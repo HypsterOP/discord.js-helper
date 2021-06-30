3d5af748069936bb0b8e3ecd8619b704b9a74867
BuildTools <jugdiscord@gmail.com>
2021-06-30 13:20:03 +0530

    fix embedbuilder not working according to readme

--- data/functions/embedBuilder.js
+++ data/functions/embedBuilder.js
@@ -3,9 +3,9 @@ const colors = require('colors')
 module.exports = EmbedBuilder;
 
 
-function EmbedBuilder(message) {
+async function EmbedBuilder(message) {
     if(!message) throw new ReferenceError("'message' is not passed in as the argument! [discord.js-helper]".red)
     const av = message.author.displayAvatarURL({ dynamic : true })
     const { MessageEmbed } = require('discord.js')
-    return new MessageEmbed().setFooter(message.author.username, av).setColor('RANDOM').setTimestamp();
+    return await message.channel.send(new MessageEmbed().setFooter(message.author.username, av).setColor('RANDOM').setimestamp());
 }
